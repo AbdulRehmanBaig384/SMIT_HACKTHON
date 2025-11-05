8d829775d4073397a6f3cc293722f1f98ac4efdc
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-11-05 10:16:39 +0500

    Refactor password input element in Register component

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -173,10 +173,7 @@ const Register = () => {
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                   <Lock className="h-5 w-5 text-gray-400" />
                 </div>
-                <input
-                  id="password"
-                  name="password"
-                  type={showPassword ? 'text' : 'password'}
+                <input id="password" name="password" type={showPassword ? 'text' : 'password'}
                   autoComplete="new-password"
                   required
                   value={formData.password}
