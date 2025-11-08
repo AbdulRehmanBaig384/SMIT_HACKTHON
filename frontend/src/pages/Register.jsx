de7989ae6e8718e4e7eaffba4969f4ae5cadbf66
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-11-08 20:11:35 +0500

    Refactor terms checkbox input in Register component

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -215,9 +215,7 @@ const Register = () => {
 
             {/* Terms and Conditions */}
             <div className="flex items-center">
-              <input id="terms" name="terms"
-                type="checkbox"
-                required
+              <input id="terms" name="terms" type="checkbox" required
                 className="h-4 w-4 text-primary-600 focus:ring-primary-500 border-gray-300 rounded" />
               <label htmlFor="terms" className="ml-2 block text-sm text-gray-700">
                 {isUrdu 
